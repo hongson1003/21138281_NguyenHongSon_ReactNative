75614918422876bbe30e2af78c2ebb221db965f9
Hongsonnguyen <hongsonnguyen1003@gmail.com>
2024-09-16 12:46:06 +0700

    add tailwinds

--- react_native/my-app/app/_layout.jsx
+++ react_native/my-app/app/_layout.jsx
@@ -1,4 +1,10 @@
 import { Stack } from 'expo-router';
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
 
 export default function Layout() {
   return <Stack />;
--- react_native/my-app/app/index.jsx
+++ react_native/my-app/app/index.jsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function Home() {
   return (
     <View className="flex-1 justify-center items-center bg-violet-400">
-      <Text className="text-white text-xl font-bold">Hello from MyComponent!</Text>
+      <Text className="text-white text-xl font-bold">Hello from MyComponent!!??</Text>
     </View>
   );
 }
