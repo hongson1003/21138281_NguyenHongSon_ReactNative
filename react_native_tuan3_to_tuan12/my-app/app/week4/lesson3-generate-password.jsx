005ca2e588d829232ac7e53ff4a3dfed254a1211
Hongsonnguyen <hongsonnguyen1003@gmail.com>
2024-09-16 14:45:17 +0700

    modifed name folder tuan3_to_tuan12

---
+++ react_native_tuan3_to_tuan12/my-app/app/week4/lesson3-generate-password.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const GeneratePassword = () => {
+  return (
+    <View>
+      <Text>Generate Password</Text>
+    </View>
+  );
+};
+
+export default GeneratePassword;
