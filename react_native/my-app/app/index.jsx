db9ca61f717c13c21141f6e8be3e81c66acc2a0f
Hongsonnguyen <hongsonnguyen1003@gmail.com>
2024-09-16 11:57:33 +0700

    add tuan 3

---
+++ react_native/my-app/app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View className="flex-1 justify-center items-center bg-violet-400">
+      <Text className="text-white text-xl font-bold">Hello from MyComponent!</Text>
+    </View>
+  );
+}
---
+++ react_native/my-app/app/week3/bai1.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const Lesson1 = () => {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className='text-violet-950'>Đây là bài 1</Text>
+        </View>
+    )
+}
+
+export default Lesson1
